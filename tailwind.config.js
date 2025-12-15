10e366e4d725fef3e7b16ead33d874c06aa198d4
Nikita Krivda <n_krivda@wargaming.net>
2025-12-15 19:57:34 +0100

    Restore Tailwind classes for patience message and add safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     './myapp/templates/**/*.html',
     './static/js/**/*.js'
   ],
+  // Safelist classes used in dynamic JS that Tailwind might miss
+  safelist: [
+    // Patience message classes (utils.js)
+    'from-emerald-100',
+    'to-teal-100',
+    'border-emerald-400',
+    'bg-emerald-500',
+    'text-emerald-800',
+    'text-emerald-700',
+    'animate-fade-in',
+  ],
   theme: {
     extend: {
       width: {
