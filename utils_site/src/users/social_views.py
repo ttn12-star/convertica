8f21acd32b0c0ae05e14e0dc3ac85b4a18294d60
Nikita Krivda <n_krivda@wargaming.net>
2025-12-20 00:53:52 +0100

    Fix code in registration

--- utils_site/src/users/social_views.py
+++ utils_site/src/users/social_views.py
@@ -1,14 +1,22 @@
 import urllib.parse
 
+from allauth.socialaccount.models import SocialApp
 from django.conf import settings
+from django.contrib.sites.models import Site
 from django.shortcuts import redirect
 
 
 class GoogleDirectOAuthView:
     def __init__(self):
-        self.client_id = (
-            "542121284706-gnpklp992hic5tni5k9ffqum4q8gsk9c.apps.googleusercontent.com"
-        )
+        # Get Google OAuth app from database
+        try:
+            site = Site.objects.get(id=settings.SITE_ID)
+            google_app = SocialApp.objects.get(provider="google", sites=site)
+            self.client_id = google_app.client_id
+        except (SocialApp.DoesNotExist, Site.DoesNotExist):
+            # Fallback to hardcoded value
+            self.client_id = "542121284706-gnpklp992hic5tni5k9ffqum4q8gsk9c.apps.googleusercontent.com"
+
         # Use dynamic redirect URI based on current site
         self.redirect_uri = f"{settings.SITE_URL}/accounts/google/login/callback/"
         self.scope = "openid email profile"
